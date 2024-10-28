a65240cf2108d77afedf40fe38d2823349c32a37
javier.gonzalezprado <javier.gonzalezprado@027P133C.wirtz.local>
2024-10-28 22:19:24 +0100

    COPIA

--- Tema4/src/main/java/tema4/JgpT04E01a.java
+++ Tema4/src/main/java/tema4/JgpT04E01a.java
@@ -2,7 +2,6 @@
  * 4.1. Realizar un programa al que se le introduzca una cadena por teclado y haga lo siguiente: 
  * Mostrar por pantalla el contenido de la cadena pasada a mayúsculas y minúsculas
  */
-
 package tema4;
 
 import java.util.Scanner;
@@ -10,6 +9,13 @@ import java.util.Scanner;
 public class JgpT04E01a {
 
     public static void main(String[] args) {
-        
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce una cadena de texto: ");
+        String letras = teclado.nextLine();
+
+        System.out.println("En mayusculas " + letras.toUpperCase());
+
+        System.out.println("En minusculas " + letras.toLowerCase());
     }
 }
