2616f9421c1adfcf791908939f5454023fe94240
javier.gonzalezprado <javier.gonzalezprado@027P133C.wirtz.local>
2024-11-15 19:01:35 +0100

    COPIA

---
+++ Tema5Repaso/src/main/java/tema5repaso/JgpT05Rep01.java
@@ -0,0 +1,18 @@
+/*
+ * 5.1. Realizar funciones que realicen los siguientes supuestos, y un main() desde el que se llame a 
+dichas funciones: 
+
+• Función llamada par() que se le pasa un entero y devuelve true si es par, false si no lo 
+es. 
+ 
+ */
+package tema5repaso;
+
+import java.util.Scanner;
+
+public class JgpT05Rep01 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+    }
+}
