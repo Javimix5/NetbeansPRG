bfe34618d313020b9a134976135d6da7b3011432
javier.gonzalezprado <javier.gonzalezprado@027P133C.wirtz.local>
2024-12-05 21:53:26 +0100

    COPIA

--- Tema6/src/main/java/tema6/JgpT6e15Examen.java
+++ Tema6/src/main/java/tema6/JgpT6e15Examen.java
@@ -8,7 +8,7 @@ un alumno para las 20 preguntas (a,b,c,d o bien z, siendo respuesta en blanco) y
 puntuación en el examen. Hacer un programa que permita primero informar de las respuestas 
 correctas a un examen y después introducir las respuestas de los alumnos, mostrándonos la nota 
 obtenida por los mismos. 
-*/
+ */
 package tema6;
 
 import java.util.Scanner;
@@ -20,14 +20,18 @@ import java.util.Scanner;
 public class JgpT6e15Examen {
 
     static Scanner teclado = new Scanner(System.in, "ISO-8859-1");
+
     public static void main(String[] args) {
+
         Examen exame = new Examen("abcdaabdcddbcaccabda", 0.5, 0.2);
-System.out.println("Introduce as respotas do alumno. Tenen que ser 20. Emprega x para marcar as preguntas non contestadas.");
-String respostasAlumno = teclado.nextLine();
-while (!exame.checkFormat(respostasAlumno)) {
-    System.out.println("O formato das respostas non e correcto. Introducela de novo.");
-    respostasAlumno = teclado.nextLine();
-}
-System.out.printf("Nota do alumno: %.2f", exame.calcularNota(respostasAlumno));
+
+        System.out.println("Introduce las respuestas del alumno. Tienen que ser 20. Usa x para marcar las preguntas no contestadas.");
+        String respostasAlumno = teclado.nextLine();
+
+        while (!exame.checkFormat(respostasAlumno)) {
+            System.out.println("El formato de las respuestas no es correcto. Introducela de nuevo.");
+            respostasAlumno = teclado.nextLine();
+        }
+        System.out.printf("Nota del alumno: %.2f", exame.calcularNota(respostasAlumno));
     }
 }
